b87056fcaca6fd1dc1d797199b81f8f980b0a335
rajatdora420 <91836266+rajatdora420@users.noreply.github.com>
2025-04-15 12:08:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ const payload = {
   username: username,
   amount: parseFloat(totalCredit.toFixed(2)),
   method: "Manual #6"
-  memo: "Auto Payment | TxID: " + txid
+  memo: `Auto Payment | TxID: ${txid}`,
 };
 
     const addRes = await axios.post("https://jinglesmm.com/adminapi/v2/payments/add", payload, {
